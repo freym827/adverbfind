8119d002ad8b48f0b662dcf65937d65fcac2041f
Bitter Starglow <freym827@gmail.com>
2018-11-27 22:35:57 -0500

    fixes

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ $(".sbtn").on("click", function() {
     var adstring = "Deleted words: "
     var nadstring = "New Text: "
     for(i=0;i<arr.length;i++) {
-        if(arr[i].toLowerCase() == "very" || arr[i].toLowerCase() == "so") {
+        if(arr[i].toLowerCase() == "very" || arr[i].toLowerCase() == "so" || arr[i].toLowerCase() == "much") {
             adv.push(arr[i])
             adstring += arr[i] + " "
         }else if(arr[i].charAt(arr[i].length-1) == "y" && arr[i].charAt(arr[i].length-2) == "l") {
             adv.push(arr[i])
             adstring += arr[i] + " "
+        }else if(arr[i].charAt(arr[i].length-1) == "," || arr[i].charAt(arr[i].length-1) == ".") {
+            if(arr[i].charAt(arr[i].length-2) == "y" && arr[i].charAt(arr[i].length-3) == "l") {
+                adv.push(arr[i])
+                adstring += arr[i] + " "
+            }else if(arr[i].toLowerCase().substring(0, arr[i].length-1) == "very" || arr[i].toLowerCase().substring(0, arr[i].length-1) == "so" || arr[i].toLowerCase().substring(0, arr[i].length-1) == "much") {
+                adv.push(arr[i])
+                adstring += arr[i] + " "
+            }else {
+                notAdv.push(arr[i])
+                nadstring += arr[i] + " "
+            }
         }else {
             notAdv.push(arr[i])
             nadstring += arr[i] + " "
