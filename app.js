be94e09316aa3e32e4d68d29422538109d97f723
Bitter Starglow <freym827@gmail.com>
2018-11-29 20:11:13 -0500

    punctuation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var nonAdArr = ["silly", "early", "bubbly", "burly", "chilly", "costly", "cuddly", "tally", "sly", "bully", "monopoly",
                     "family", "ally", "anomoly", "comply", "belly", "supply", "rely", "rally", "jelly", "lily", "curly",
                 "holy", "hourly", "ugly", "imply"]
+var adArr = ["very", "so", "much"]
 
 $(".sbtn").on("click", function() {
     $(".t2").val("")
@@ -8,14 +9,115 @@ $(".sbtn").on("click", function() {
     var arr = para.split(" ")
     var adstring = "Deleted words: "
     var nadstring = "New Text: "
+    
     for(i=0;i<arr.length;i++) {
         if(!(nonAdArr.indexOf(arr[i].toLowerCase()) == -1)) {
             nadstring += arr[i] + " "
-        }else if(arr[i].toLowerCase() == "very" || arr[i].toLowerCase() == "so" || arr[i].toLowerCase() == "much") {
+        }else if(adArr.includes(arr[i].toLowerCase())) {
             adstring += arr[i] + " "
         }else if(arr[i].charAt(arr[i].length-1) == "y" && arr[i].charAt(arr[i].length-2) == "l") {
             adstring += arr[i] + " "
-        }else if(arr[i].charAt(arr[i].length-1) == "," || arr[i].charAt(arr[i].length-1) == "." || arr[i].charAt(arr[i].length-1) == "!" || arr[i].charAt(arr[i].length-1) == "?" || arr[i].charAt(arr[i].length-1) == ";" || arr[i].charAt(arr[i].length-1) == ":") {
+        }else if(arr[i].indexOf(".") > 0) {
+            var endofword = arr[i].indexOf(".")
+            var temp = arr[i].substring(0, endofword)
+            if(!(nonAdArr.indexOf(temp.toLowerCase()) == -1)) {
+                nadstring += arr[i] + " "
+            }else if(adArr.includes(temp.toLowerCase())) {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else if(temp.charAt(temp.length-1) == "y" && temp.charAt(temp.length-2) == "l") {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else {
+                nadstring += arr[i] + " "
+            }
+        }else if(arr[i].indexOf(",") > 0) {
+            var endofword = arr[i].indexOf(",")
+            var temp = arr[i].substring(0, endofword)
+            if(!(nonAdArr.indexOf(temp.toLowerCase()) == -1)) {
+                nadstring += arr[i] + " "
+            }else if(adArr.includes(temp.toLowerCase())) {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else if(temp.charAt(temp.length-1) == "y" && temp.charAt(temp.length-2) == "l") {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else {
+                nadstring += arr[i] + " "
+            }
+        }else if(arr[i].indexOf("!") > 0) {
+            var endofword = arr[i].indexOf("!")
+            var temp = arr[i].substring(0, endofword)
+            if(!(nonAdArr.indexOf(temp.toLowerCase()) == -1)) {
+                nadstring += arr[i] + " "
+            }else if(adArr.includes(temp.toLowerCase())) {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else if(temp.charAt(temp.length-1) == "y" && temp.charAt(temp.length-2) == "l") {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else {
+                nadstring += arr[i] + " "
+            }
+        }else if(arr[i].indexOf("?") > 0) {
+            var endofword = arr[i].indexOf("?")
+            var temp = arr[i].substring(0, endofword)
+            if(!(nonAdArr.indexOf(temp.toLowerCase()) == -1)) {
+                nadstring += arr[i] + " "
+            }else if(adArr.includes(temp.toLowerCase())) {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else if(temp.charAt(temp.length-1) == "y" && temp.charAt(temp.length-2) == "l") {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else {
+                nadstring += arr[i] + " "
+            }
+        }else if(arr[i].indexOf(";") > 0) {
+            var endofword = arr[i].indexOf(";")
+            var temp = arr[i].substring(0, endofword)
+            if(!(nonAdArr.indexOf(temp.toLowerCase()) == -1)) {
+                nadstring += arr[i] + " "
+            }else if(adArr.includes(temp.toLowerCase())) {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else if(temp.charAt(temp.length-1) == "y" && temp.charAt(temp.length-2) == "l") {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else {
+                nadstring += arr[i] + " "
+            }
+        }
+        else if(arr[i].indexOf(":") > 0) {
+            var endofword = arr[i].indexOf(":")
+            var temp = arr[i].substring(0, endofword)
+            if(!(nonAdArr.indexOf(temp.toLowerCase()) == -1)) {
+                nadstring += arr[i] + " "
+            }else if(adArr.includes(temp.toLowerCase())) {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else if(temp.charAt(temp.length-1) == "y" && temp.charAt(temp.length-2) == "l") {
+                adstring += temp + " "
+                nadstring += arr[i].substring(endofword, arr[i].length)
+            }else {
+                nadstring += arr[i] + " "
+            }
+        }else {
+            nadstring += arr[i] + " "
+        }
+    }
+
+    $(".t2").val(adstring + "\n\n\n" + nadstring)
+    
+});
+
+$(".ibtn").on("click", function() {
+    var ig = $(".ignore").val().trim().toLowerCase()
+    nonAdArr.push(ig)
+    $(".igwords").append(" " + ig)
+})
+
+ /*else if(arr[i].charAt(arr[i].length-1) == "," || arr[i].charAt(arr[i].length-1) == "." || arr[i].charAt(arr[i].length-1) == "!" || arr[i].charAt(arr[i].length-1) == "?" || arr[i].charAt(arr[i].length-1) == ";" || arr[i].charAt(arr[i].length-1) == ":") {
             if(!(nonAdArr.indexOf(arr[i].substring(0,arr[i].length-1).toLowerCase()) == -1)) {
                 nadstring += arr[i] + " "
             }else if(arr[i].charAt(arr[i].length-2) == "y" && arr[i].charAt(arr[i].length-3) == "l") {
@@ -33,17 +135,11 @@ $(".sbtn").on("click", function() {
             }else {
                 nadstring += arr[i] + " "
             }
-        }else {
-            nadstring += arr[i] + " "
-        }
-    }
+        }*/
 
-    $(".t2").val(adstring + "\n\n\n" + nadstring)
-    
-});
-
-$(".ibtn").on("click", function() {
-    var ig = $(".ignore").val().trim()
-    nonAdArr.push(ig)
-    $(".igwords").append(" " + ig)
-})
+/*Hello everybody. Recently I stupidly thought this couldn't happen so soon. 
+Is it chilly in here? I am so happy to be here though. I am extremely grateful for this opportunity. 
+I am seriously thrilled. It really means the world to me. I love you all so much. 
+This is all so extravagantly done. I really don't know what to do. 
+I think I am going to cry heavily, with how this situation miraculously unfolded. 
+This is all so heavenly. Thank you muchly! */
