cea3ee470090cde34c1ea98719c4c3022108d97d
Bitter Starglow <freym827@gmail.com>
2019-02-07 16:45:56 -0500

    stuff

--- app.js
+++ app.js
@@ -64,7 +64,27 @@ $(".ibtn").on("click", function() {
     $(".igwords").append(" " + ig)
 })
 
+$("a").on('click', function(event) {
 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+});
 
 /*Hello everybody. Recently I stupidly thought this couldn't happen so soon. 
 Is it chilly in here? Good thing I brought this burly coat! I am so happy to be here though. I am extremely grateful for this opportunity. 
